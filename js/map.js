e4218a17e87ccb9e11d2fd4332948f2ced463dee
Jonathan Beliën <dev@jbelien.be>
2020-12-01 11:18:45 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,15 +1,17 @@
-$(document).ready(function() {
+(function () {
   var map = L.map("map").setView([0, 0], 2);
 
   L.tileLayer("https://tile.geo6.be/osmbe/{z}/{x}/{y}.png", {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles courtesy of <a href="https://geo6.be/">GEO-6</a>.',
-    maxZoom: 18
+    attribution: [
+      '&copy; <a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+      'Tiles courtesy of <a href="https://geo6.be/">GEO-6</a>.',
+    ].join(", "),
+    maxZoom: 18,
   }).addTo(map);
 
   L.marker([50.896349, 4.36059], {
-    interactive: false
+    interactive: false,
   }).addTo(map);
 
-  map.setView([50.896349, 4.36059], 13);
-});
+  map.setView([50.896349, 4.36059], 11);
+})();
